5641287b25f0d81d98176402f791aff51be64c22
johnraisor <johnraisor@mail.weber.edu>
2018-12-12 11:40:33 -0700

    object not null

---
+++ Bowl.h
@@ -0,0 +1,18 @@
+//
+// Created by johnraisor on 12/12/2018.
+//
+
+#ifndef BOWLING_BOWL_H
+#define BOWLING_BOWL_H
+
+
+class Bowl {
+public:
+    void AddFrame( int s1, int s2)
+    {
+
+    }
+};
+
+
+#endif //BOWLING_BOWL_H
--- main.cpp
+++ main.cpp
@@ -2,10 +2,17 @@
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
+#include "Bowl.h"
 
-TEST_CASE( "")
+TEST_CASE( "have game", "bowl")
 {
-    // Arrange
-    // Act
-    // Assert
+    Bowl *b = new Bowl;
+    REQUIRE( b != NULL);
+}
+
+TEST_CASE( "game add score", "bowl")
+{
+    Bowl *b = new Bowl;
+    b->AddFrame( 3, 5);
+    REQUIRE( b != NULL);
 }
